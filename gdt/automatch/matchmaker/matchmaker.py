73003256f6c4c63d6c2af8c0e281faa361248c2d
VirtualBoxImages.com <adminuser@debian-i386-vdi>
2015-01-15 19:43:33 -1000

    The great purge of every unneeded file (reducing clutter)

--- gdt/automatch/matchmaker/matchmaker.py
+++
@@ -1,8 +0,0 @@
-
-# A matchmaker generates Match objects from Seek objects. This is an abstract
-# class.
-class Matchmaker():
-
-    # Returns Match objects generated from the given Seek objects
-    def generate_matches(self, seeks):
-        return NotImplemented
--- misc/db_scripts/dump_initdata.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-pg_dump -a -t advbot goko > misc/db_initdata/initdata-advbots.sql
-pg_dump -a -t bot goko > misc/db_initdata/initdata-bots.sql
-pg_dump -a -t card_url goko > misc/db_initdata/initdata-card_url.sql
-pg_dump -a -t ts_system goko > misc/db_initdata/initdata-ts_system.sql
